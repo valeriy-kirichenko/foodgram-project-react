280c2f6aa63c874f8e05ce5df8cae7c01ef4add5
Valeriy Kirichenko <vmkirichenko1991@yandex.ru>
2022-07-30 23:24:10 +0300

    After 2d review

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,5 @@
 from django.db.models import F
 from djoser.serializers import UserSerializer
-from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
