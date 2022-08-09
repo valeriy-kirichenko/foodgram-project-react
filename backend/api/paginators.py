ed46c6d3c0585cc8523ed860a680d4ab6af76883
Valeriy Kirichenko <vmkirichenko1991@yandex.ru>
2022-08-09 16:18:20 +0300

    Add paginator docstring

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -2,5 +2,10 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPageNumberPagination(PageNumberPagination):
+    """Разбивает список рецептов на страницы.
+
+    Пользователь может настраивать количество выводимых на страницу рецептов
+    указав значение параметра limit в запросе.
+    """
     page_size = 6
     page_size_query_param = 'limit'
